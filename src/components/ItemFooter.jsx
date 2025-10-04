5382b1e70d620fe215554967bf0a398c1d00763a
ArnutDev <arnutbuadonpai@gmail.com>
2025-10-04 15:02:06 +0700

    Fixed UI

--- src/components/ItemFooter.jsx
+++ src/components/ItemFooter.jsx
@@ -1,35 +1,42 @@
-
-export default function ItemFooter({ specialCount,itemDetails,footerMessage }) {
+export default function ItemFooter({ specialCount, itemDetails, footerMessage }) {
   return (
     <footer className="mt-3 bg-dark text-center">
       <div className="container">
-        <table className="table table-bordered table-sm my-0">
+        <table className="table table-bordered table-sm my-0 text-center" style={{ tableLayout: "fixed", width: "100%" }}>
           <thead className="table-dark">
             <tr>
               <th colSpan={specialCount.length}>{footerMessage}</th>
             </tr>
           </thead>
+
           <tbody>
+            {/* แถวภาพ */}
             <tr>
               {itemDetails?.map((item, idx) => (
-                <td key={idx}>
+                <td key={idx} className="align-middle">
                   <img
-                    className="py-2" 
                     src={item.Image}
                     alt={item.Name}
-                    style={{ width: "55px", height: "100px" }}
+                    className="img-fluid mx-auto d-block object-fit-contain py-2"
+                    style={{ maxHeight: "80px" }}
                   />
                 </td>
               ))}
             </tr>
+
+            {/* แถวจำนวน */}
             <tr>
               {specialCount.map((count, idx) => (
-                <td key={idx} className="py-2">{count}</td>
+                <td key={idx} className="py-2 align-middle">
+                  {count}
+                </td>
               ))}
             </tr>
+
+            {/* แถวชื่อ */}
             <tr>
               {itemDetails?.map((item, idx) => (
-                <td key={idx} className="py-2">
+                <td key={idx} className="py-2 align-middle">
                   {item.Name}
                 </td>
               ))}
